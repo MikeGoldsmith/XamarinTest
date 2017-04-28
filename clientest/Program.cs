80e8e2c9411f0e89ecf4dfb57f5e0dd72a0d50fb
Mike Goldsmith <goldsmith.mike@gmail.com>
2017-04-28 12:12:24 +0100

    add sln and project

---
+++ clientest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Couchbase;
+using Couchbase.Configuration.Client;
+using Newtonsoft.Json;
+
+namespace clientest
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var config = new ClientConfiguration
+			{
+				Servers = new List<Uri> { new Uri("http://10.112.161.101") }
+			};
+			var cluster = new Cluster(config);
+			var bucket = cluster.OpenBucket("default");
+
+			bucket.Upsert("test", new Person("mike"));
+			var getResult = bucket.Get<Person>("test");
+
+			Console.WriteLine(JsonConvert.SerializeObject(getResult.Value));
+		}
+	}
+
+	class Person
+	{
+		public string Name { get; set; }
+
+		public Person(string name)
+		{
+			Name = name;
+		}
+	}
+}
